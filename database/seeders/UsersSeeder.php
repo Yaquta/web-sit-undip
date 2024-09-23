10e3901c32893ba26abdeb6a5c76af3515b5cea5
Ferituaku <ferismegg123@gmail.com>
2024-09-23 20:49:36 +0700

    update1

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -33,8 +33,5 @@ class UsersSeeder extends Seeder
                 'password' => bcrypt('123')
             ]
         ];
-        foreach ($userData as $key => $val) {
-            User::create($val);
-        }
     }
 }
